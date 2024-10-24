8ccfad67dafafefbc27e82a86939c7f7bbae7848
ringoXD <nep360i@gmail.com>
2024-10-24 07:32:57 +0000

    progress

---
+++ extensions/hoge.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+# cogの読み込み
+class __Cog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+        # cogの起動処理
+        @commands.Cog.listener()
+        async def on_ready(self):
+            print("Cog ready!")
+
+        @app_commands.command(name="example", description="test")
+        async def est(interaction: discord.Interaction):
+            await interaction.response.send_message('It works! ✨') 
+
+async def setup(bot):
+    print('hoge is ready 🔥')
--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import discord
 from discord.ext import commands
+import discord.app_commands
 from constant import TOKEN
 
+intents = discord.Intents.all()
+
+client = discord.Client(intents=discord.Intents.all())
+
+# discordとbotの接続
+intents = discord.Intents.default()
+intents.message_content = True
+
+# クライアントの作成
+client = discord.Client(intents=intents)
+
+# コマンドツリーの作成
+tree = discord.app_commands.CommandTree(client)
+
+# 起動イベント
+@client.event
+async def on_ready():
+    print("Ready 🚀")
+
 extensions = (
+    "extensions.hoge"
+    # "extensions.huga",
 )
 
+# コマンドの同期処理
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(
@@ -13,8 +36,9 @@ class MyBot(commands.Bot):
         )
 
     async def setup_hook(self):
+        # await self.tree.sync(guild=None)
         for extension in extensions:
-            await self.load_extension(f'extensions.{extension}')
+            await self.load_extension(extensions)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyBot().run(TOKEN)
